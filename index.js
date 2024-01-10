45f87d4947cb8a1ab78856de1fc462b9ee2cbb73
Jaffdavy <jaffdavy100@gmail.com>
2024-01-10 11:59:28 +0100

    compplet

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const boxes = document.querySelectorAll('.box')
 
-window.addEventListener('scroll', checkBoxes)
-checkBoxes()
+window.addEventListener ('scroll', checkBoxes)
+checkBoxes ()
 
-function checkBoxes(){
+function checkBoxes () {
   const triggerBottom = window.innerHeight / 5 * 4
-    boxes.forEach(box => {
-      const boxTop = box.getBoundingClientRect().top
-        if(boxTop < triggerBottom) {
-          box.classList.add('show')
-        } else {
-            box.classList.remove('show')
-        }
-    })
+  boxes.forEach (box => {
+    const boxTop = box.getBoundingClientRect().top
+    if (boxTop < triggerBottom) {
+      box.classList.add('show')
+    } else {
+      box.classList.remove('show')
+    }
+  })
 }
